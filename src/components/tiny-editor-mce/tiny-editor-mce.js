bd3d255ae270288676b282012d3a98ce06ad936f
okletmecode <156535856+okletmecode@users.noreply.github.com>
2024-01-17 05:28:20 +0000

    detecting strong and all tags now

--- src/components/tiny-editor-mce/tiny-editor-mce.js
+++ src/components/tiny-editor-mce/tiny-editor-mce.js
@@ -19,8 +19,8 @@ const TinyMceEditor = () => {
 
   useEffect(() => {
     setdata(localStorage.getItem("theme"))
-  
   }, [])
+  
   
 
   const ArticlesData = useSelector((state) => state.ArticlesData);
@@ -170,7 +170,8 @@ console.log(obj)
           // change theme from here
           skin: localStorages  == "dark" ? "oxide-dark": "oxide",
           content_css: localStorages == "dark" ? "dark" : "default",
-
+          quickbars_selection_toolbar: 'bold italic | quicklink h2 h3 blockquote quickimage quicktable',
+                
           height: 500,
           menubar: false,
           plugins: [
@@ -195,7 +196,29 @@ console.log(obj)
             "emoticons",
             "template",
             "help",
-          ],
+          ],  
+            valid_elements : "@[id|class|style|title|lang|xml::lang|onclick|ondblclick|"
+          + "onmousedown|onmouseup|onmouseover|onmousemove|onmouseout|onkeypress|"
+          + "onkeydown|onkeyup],a[rel|rev|charset|hreflang|tabindex|accesskey|type|"
+          + "name|href|target|title|class|onfocus|onblur],strong/b,em/i,strike,u,"
+          + "#p,-ol[type|compact],-ul[type|compact],-li,br,img[longdesc|usemap|"
+          + "src|border|alt=|title|hspace|vspace|width|height|align],-sub,-sup,"
+          + "-blockquote,-table[border=0|cellspacing|cellpadding|width|frame|rules|"
+          + "height|align|summary|bgcolor|background|bordercolor],-tr[rowspan|width|"
+          + "height|align|valign|bgcolor|background|bordercolor],tbody,thead,tfoot,"
+          + "#td[colspan|rowspan|width|height|align|valign|bgcolor|background|bordercolor"
+          + "|scope],#th[colspan|rowspan|width|height|align|valign|scope],caption,-div,"
+          + "-code,-pre,address,-h1,-h2,-h3,-h4,-strong ,-h5,-h6,hr[size|noshade],-font[face"
+          + "|size|color],dd,dl,dt,cite,abbr,acronym,del[datetime|cite],ins[datetime|cite],"
+          + "object[classid|width|height|codebase|*],param[name|value|_value],embed[type|width"
+          + "|height|src|*],script[src|type],map[name],area[shape|coords|href|alt|target],bdo,"
+          + "button,col[align|char|charoff|span|valign|width],colgroup[align|char|charoff|span|"
+          + "valign|width],dfn,fieldset,form[action|accept|accept-charset|enctype|method],"
+          + "input[accept|alt|checked|disabled|maxlength|name|readonly|size|src|type|value],"
+          + "kbd,label[for],legend,noscript,optgroup[label|disabled],option[disabled|label|selected|value],"
+          + "q[cite],samp,select[disabled|multiple|name|size],small,"
+          + "textarea[cols|rows|disabled|name|readonly],tt,var,big" , 
+    
           toolbar:
             "undo redo |  fontselect fontsizeselect formatselect | styles | bold italic | alignleft aligncenter alignright alignjustify | " +
             "bullist numlist outdent indent | link image | print preview media fullscreen | " +
